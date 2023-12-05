0f3322823796652e2f4f44779f14639fb9e4538c
d.menegon <d.menegon@GOS-VDI511>
2023-12-05 14:41:12 +0100

    first commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/_services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login(): void {
+    if (this.authService.login(this.username, this.password)) {
+      this.router.navigateByUrl('home').then(() => {
+        window.location.reload();
+      });
+    }
+  }
+}
